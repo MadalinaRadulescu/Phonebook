3db51803054a9a808eb435a92b4854778a5e85e0
MadalinaRadulescu <radulescumadalina90@gmail.com>
2022-03-29 08:45:57 +0300

    Final

--- greetings.py
+++ greetings.py
@@ -1,5 +1,18 @@
-# "What's your name?"
-# "Hello XY, I am your phone book."
+name = input("What's your name?: ")
+print("Hello " + name + ", I am your phone book")
+
+age = int(input("How old are you?:"))
+if age <= 18:
+    print("You are so young! Life is ahead of you!")
+elif age > 18 and age < 40:
+    print("That's a nice age!")
+elif age > 40:
+    print("You must be very wise!")
+elif isinstance(age,int) == False:
+    print("That doesn't seem to be an integer.")
+
+
+
 
 
 # "How old are you?"
--- phonebook.py
+++ phonebook.py
@@ -7,6 +7,44 @@ ugly_phone_list = ["98-333-111", "12--323-566", "123-34-221", "99-948-321", "-12
                      "894-58438-543", "85-234-222",
                      "12-333-444-", "99-888--433", "15-465-876", "98-555-22", "-3-355-333", "3--344-53", "--2--45---",
                      "11-111-222", "12#22$34$222", "98 223 555"]
+phone_list1 = []
+phone_list2 = []
+for item in phone_list:
+    if len(item) == 10:
+        phone_list1.append(item)
+    else:
+        phone_list2.append(item)
+print("These are the valid phone numbers in your phonebook:" + str(phone_list1))
+print("and these are the wrong ones:" + str(phone_list2))
+
+area_codes = []
+for item in phone_list1:
+    area_codes.append(item[0]+item[1])
+print("The area codes:" + str(area_codes))
+
+phone_list3 = []
+for i in phone_list1:
+    phone_list3.append(i[2:])
+print("and the numbers without the area codes:" + str(phone_list3))
+
+unic_areacodes = []
+for i in area_codes:
+    if i not in unic_areacodes:
+        unic_areacodes.append(i)
+print("The unique area codes:" + str(unic_areacodes))
+
+countx = sum(1 for i in phone_list1 if "98" in i)
+print("Phone numbers for area code 98: " + str(countx))
+
+county = sum(1 for i in phone_list1 if '34' in i)
+print("Phone numbers for area code 34: " + str(county))
+
+countz = sum(1 for i in phone_list1 if '72' in i)
+print("Phone numbers for area code 72: " + str(countz))
+
+
+
+
 
 
 # "These are the valid phone numbers in your phonebook:"
